4720fad8577a10ba4b98746409a10dd7af2098e4
trevormac <trevor.c.macgregor@gmail.com>
2017-03-06 17:29:07 -0500

    Initial Commit

---
+++ Word Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effects
+//
+//  Created by Trevor MacGregor on 2017-03-06.
+//  Copyright © 2017 Trevor MacGregor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
