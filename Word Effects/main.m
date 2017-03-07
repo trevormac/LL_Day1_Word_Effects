5b25722a3dba4f04201bead7bcd02a5c9eaca75a
trevormac <trevor.c.macgregor@gmail.com>
2017-03-07 10:40:24 -0500

    Cleaned up some of the questions.

--- Word Effects/main.m
+++ Word Effects/main.m
@@ -15,7 +15,7 @@ int main(int argc, const char * argv[]) {
         while (x == 1){
         
         char inputCharsIntro[255];
-        printf("I can do 6 things that are pretty cool. So please pick a number between 1 and 6: ");
+        printf("I can do 6 things that are pretty cool. Please pick a number between 1 and 6 and I'll show you: ");
         fgets(inputCharsIntro, 255, stdin);
         NSString *textInputIntro = [NSString stringWithUTF8String:inputCharsIntro];
         NSInteger NumIntro = [textInputIntro integerValue];
@@ -34,7 +34,7 @@ int main(int argc, const char * argv[]) {
          //2. Lowercase
             else if (NumIntro == 2) {
         char inputCharsB[255];
-        printf("Now please give me a word and I'll convert it to an lowercase string: \n");
+        printf("Please give me a word and I'll convert it to an lowercase string: \n");
         fgets(inputCharsB, 255, stdin);
         NSString *textInputB = [NSString stringWithUTF8String:inputCharsB];
         NSString *lowerCTextInput = [textInputB lowercaseString];
@@ -45,7 +45,7 @@ int main(int argc, const char * argv[]) {
         //3.Numberize
             else if (NumIntro == 3) {
         char inputCharsC[255];
-        printf("Now please give me a number and I'll convert it to an integer: \n");
+        printf("Please give me a number and I'll convert it to an integer: \n");
         fgets(inputCharsC, 255, stdin);
         NSString *textInputC = [NSString stringWithUTF8String:inputCharsC];
         NSScanner *scanner = [NSScanner scannerWithString:textInputC];
@@ -54,7 +54,7 @@ int main(int argc, const char * argv[]) {
                     NSLog(@"That is not a number");
                 }else{
         NSInteger stringToNum = [textInputC integerValue];
-        NSLog(@"%i", stringToNum);
+        NSLog(@"Integer %ld", stringToNum);
             }
             }
         
@@ -62,10 +62,10 @@ int main(int argc, const char * argv[]) {
         //4.Canadianize
             else if (NumIntro == 4) {
         char inputCharsD[255];
-        printf("Now say something and I'll Canadianize it:\n");
+        printf("Say something and I'll Canadianize it: ");
         fgets(inputCharsD, 255, stdin);
         NSString *textInputD = [NSString stringWithUTF8String:inputCharsD];
-        NSLog(@"%@ ,eh!", textInputD);
+        NSLog(@"%@,eh!", textInputD);
             }
         
         
