602bd15f13ab8e4c22775f596078260d4f5fd26f
trevormac <trevor.c.macgregor@gmail.com>
2017-03-07 09:13:19 -0500

    Added if statements and fixed #3 to include a else if statement.

--- Word Effects/main.m
+++ Word Effects/main.m
@@ -13,40 +13,64 @@ int main(int argc, const char * argv[]) {
         
         int x = 1;
         while (x == 1){
+        
+        char inputCharsIntro[255];
+        printf("I can do 6 things that are pretty cool. So please pick a number between 1 and 6: ");
+        fgets(inputCharsIntro, 255, stdin);
+        NSString *textInputIntro = [NSString stringWithUTF8String:inputCharsIntro];
+        NSInteger NumIntro = [textInputIntro integerValue];
+            
             
         //1. Uppercase
+            if (NumIntro == 1) {
         char inputCharsA[255];
-        printf("Hello, please give me your name and I'll convert it to an uppercase string: \n");
+        printf("Please give me your name and I'll convert it to an uppercase string: \n");
         fgets(inputCharsA, 255, stdin);
         NSString *textInputA = [NSString stringWithUTF8String:inputCharsA];
         NSString *upperCTextInput = [textInputA uppercaseString];
         NSLog(@"%@", upperCTextInput);
+            }
         
-        
-        //2. Lowercase
+         //2. Lowercase
+            else if (NumIntro == 2) {
         char inputCharsB[255];
         printf("Now please give me a word and I'll convert it to an lowercase string: \n");
         fgets(inputCharsB, 255, stdin);
         NSString *textInputB = [NSString stringWithUTF8String:inputCharsB];
         NSString *lowerCTextInput = [textInputB lowercaseString];
         NSLog(@"%@", lowerCTextInput);
+            }
+                    
         
         //3.Numberize
+            else if (NumIntro == 3) {
         char inputCharsC[255];
         printf("Now please give me a number and I'll convert it to an integer: \n");
         fgets(inputCharsC, 255, stdin);
         NSString *textInputC = [NSString stringWithUTF8String:inputCharsC];
-        NSInteger *stringToNum = [textInputC integerValue];
-        NSLog(@"%l", stringToNum);
+        NSScanner *scanner = [NSScanner scannerWithString:textInputC];
+        BOOL isNumeric = [scanner scanInteger:NULL] && [scanner isAtEnd];
+                if (isNumeric == false){
+                    NSLog(@"That is not a number");
+                }else{
+        NSInteger stringToNum = [textInputC integerValue];
+        NSLog(@"%i", stringToNum);
+            }
+            }
+        
         
         //4.Canadianize
+            else if (NumIntro == 4) {
         char inputCharsD[255];
-        printf("Now say something and I'll Canadianize it: \n");
+        printf("Now say something and I'll Canadianize it:\n");
         fgets(inputCharsD, 255, stdin);
         NSString *textInputD = [NSString stringWithUTF8String:inputCharsD];
         NSLog(@"%@ ,eh!", textInputD);
+            }
+        
         
         //5.Respond
+            else if (NumIntro == 5) {
         char inputCharsE[255];
         printf("Now you can say anything eg. ask me a question, yell at me, whatever: \n");
         fgets(inputCharsE, 255, stdin);
@@ -58,8 +82,11 @@ int main(int argc, const char * argv[]) {
         } else {
             NSLog(@"%@", textInputE);
         }
-        
+            }
+            
+       
         //6.De-Space-It
+            else if (NumIntro == 6) {
         char inputCharsF[255];
         printf("Now you can say anything and I'll convert the whitespaces to dashes: \n");
         fgets(inputCharsF, 255, stdin);
@@ -67,6 +94,9 @@ int main(int argc, const char * argv[]) {
         if ([textInputF containsString:@" "]) {
             NSString *dashed = [textInputF stringByReplacingOccurrencesOfString:@" " withString:@"-"];
             NSLog(@"%@", dashed);
+        }
+            
+       
         }
         }
         
