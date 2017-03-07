0b814e2edd04e4edef825246f6020b513b67c116
trevormac <trevor.c.macgregor@gmail.com>
2017-03-06 20:52:34 -0500

    Added first 2 questions

--- Word Effects/main.m
+++ Word Effects/main.m
@@ -10,8 +10,25 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        
+        //1. Uppercase
+        char inputCharsA[255];
+        printf("Hello, please give me your name and I'll convert it to an uppercase string: \n");
+        fgets(inputCharsA, 255, stdin);
+        NSString *textInputA = [NSString stringWithUTF8String:inputCharsA];
+        NSString *upperCTextInput = [textInputA uppercaseString];
+        NSLog(@"%@", upperCTextInput);
+        
+        
+        //2. Lowercase
+        char inputCharsB[255];
+        printf("Now please give me a word and I'll convert it to an lowercase string: \n");
+        fgets(inputCharsB, 255, stdin);
+        NSString *textInputB = [NSString stringWithUTF8String:inputCharsB];
+        NSString *lowerCTextInput = [textInputB lowercaseString];
+        NSLog(@"%@", lowerCTextInput);
+        
     }
-    return 0;
+        return 0;
 }
